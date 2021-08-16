4d997a869207dd417f664078c190d867de39720a
SantiagoRosasV <89027105+SantiagoRosasV@users.noreply.github.com>
2021-08-16 09:18:48 -0700

    Update exercise.py

--- assignments/01AreayVolumen/src/exercise.py
+++ assignments/01AreayVolumen/src/exercise.py
@@ -2,6 +2,12 @@ import math
 def main():
     #escribe tu código abajo de esta línea
     pass
+import math
+radio=int(input('Dame Radio:',))
+area=str(math.pi*(radio**2))
+vol=str((math.pi*(radio**3))/3)
+print('Área:' + area)
+print('Volumen:' + vol)
 
 
 
